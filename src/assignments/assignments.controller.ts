efa941927951b2f5849a425de1d17eb755720316
bsit80 <jordz3725@gmail.com>
2024-11-09 23:51:08 +0800

    Completed Assignment 1: Fibonacci Sequence

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignments')
-export class AssignmentsController {}
+export class AssignmentsController {
+  // Assignment 1: Fibonacci Sequence
+  @Get('fibonacci/:n')
+  getFibonacci(@Param('n') n: number): { sequence: number[] } {
+    const sequence = [];
+    let a = 0, b = 1;
+
+    for (let i = 0; i < n; i++) {
+      sequence.push(a);
+      [a, b] = [b, a + b];
+    }
+
+    return { sequence };
+  }
